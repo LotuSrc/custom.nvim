e4adf9cb5ea6888a6a61f58ee41dda9ef3b90316
LotuSrc <451415738@qq.com>
2023-02-03 11:37:00 +0800

    init

---
+++ plugins/init.lua
@@ -0,0 +1,21 @@
+return {
+
+  ----------------------------------------- default plugins ------------------------------------------
+  
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+
+  --------------------------------------------- custom plugins ----------------------------------------------
+
+  -- format & linting
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.null-ls"
+    end,
+  },
+}
