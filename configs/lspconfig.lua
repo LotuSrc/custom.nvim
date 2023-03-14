2aa4b98d8ca44516be27fbc62e6746a9cb10f21d
LotuSrc <451415738@qq.com>
2023-03-14 15:09:07 +0800

    feat: change to v2.0

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -2,6 +2,8 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
 local servers = { "pyright", "clangd" }
 
 for _, lsp in ipairs(servers) do
